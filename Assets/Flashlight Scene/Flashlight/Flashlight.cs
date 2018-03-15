6208b1566b1ee543e47f67a4b961c72d1182fbdb
kodyleuang <kodyleuang@gmail.com>
2018-03-15 16:29:23 -0500

    Created working flashlight
    - Not VR ready
    - Batteries Required
    - Press "F" for flashlight, needs batteries to work

--- Assets/Flashlight Scene/Flashlight/Flashlight.cs
+++ Assets/Flashlight Scene/Flashlight/Flashlight.cs
@@ -4,13 +4,69 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour {
 
+    private bool flashlightEnabled;
+    public GameObject flashlight;
+    public GameObject lightObj;
+    public float maxEnergy;
+    private float currentEnergy;
+
+    private int batteries;
+    private GameObject batteryPickedUp;
+    private float usedEnergy;
+
 	// Use this for initialization
 	void Start () {
-		
+        maxEnergy = 50 * batteries;
+        currentEnergy = maxEnergy;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// Update is called once per second
+	void FixedUpdate () {
+        maxEnergy = 50 * batteries;
+        currentEnergy = maxEnergy;
+        //equip flashlight with "F" key
+        if (Input.GetKeyDown(KeyCode.F)) {
+            flashlightEnabled = !flashlightEnabled;
+        }
+
+        //flashlight on if there is energy in it
+        if (flashlightEnabled)
+        {
+            flashlight.SetActive(true);
+
+            if (currentEnergy <= 0)
+            {
+                lightObj.SetActive(false);
+                batteries = 0;
+            }
+
+            if (currentEnergy > 0)
+            {
+                lightObj.SetActive(true);
+                currentEnergy -= 0.5f * Time.deltaTime;
+                usedEnergy += .5f * Time.deltaTime;
+            }
+
+            if (usedEnergy >= 50)
+            {
+                batteries -= 1;
+                usedEnergy = 0;
+            }
+        }
+        //Flashlight off
+        else
+        {
+            flashlight.SetActive(false);
+        }
 	}
+
+    //Picks up and and adds batteries when stepped over
+    public void OnTriggerEnter(Collider batt){
+        if (batt.tag == "Battery")
+        {
+            batteryPickedUp = batt.gameObject;
+            batteries += 1;
+            Destroy(batteryPickedUp);
+        }
+    }
 }
