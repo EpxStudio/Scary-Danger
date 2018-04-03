d9e1ecd4e85745d3501ebf47d8ebe42b10023d9f
Kody Leuang <kodyleuang@gmail.com>
2018-04-03 15:41:12 -0500

    Imported Flashlight Assets

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour
+{
+    public bool trigger;
+    LineRenderer line;
+    public float m_MaxDamage = 1f;
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+    void FixedUpdate()
+    {
+        if (trigger)
+        {
+
+        }
+    }
+    public void ToggleSwitch()
+    {
+        trigger = !trigger;
+        if (trigger)
+            FixedUpdate();
+        else
+            FixedUpdate();
+    }
+}
