06f60811f113fb60949fada6d1ef4485a4b70481
Kody Leuang <kodyleuang@gmail.com>
2018-04-13 18:44:40 -0500

    Revert "Revert "Hp merging into Master""
    This reverts commit 45569137c587666ab3cbf1006246601d99a0ce56.

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+    //Flashlight Management variables
+    private bool inHands;
+    private bool flashlightEnabled;
+    public GameObject lightObj;
+    public float maxEnergy;
+    private float currentEnergy;
+
+    //Battery interaction variables
+    private int batteries;
+    private float usedEnergy;
+    
+    // Use this for initialization
+    void Start () {
+        maxEnergy = 50 * batteries;
+        currentEnergy = maxEnergy;
+	}
+
+	
+	// Update is called 50 times per second
+	void FixedUpdate () {
+        maxEnergy = 50 * batteries;
+        currentEnergy = maxEnergy;
+        //Flashlight on if there is energy in it and it is in the players hands
+        //Bug: Player does not need to be holding the flashlight only needs to be in the box collider
+        if (flashlightEnabled)
+        {
+            lightObj.SetActive(true);
+            if (currentEnergy <= 0)
+            {
+                lightObj.SetActive(false);
+                batteries = 0;
+            }
+
+            if (currentEnergy > 0)
+            {
+                lightObj.SetActive(true);
+                currentEnergy -= .5f * Time.deltaTime;
+                usedEnergy += .5f * Time.deltaTime;
+            }
+
+            if (usedEnergy >= 50)
+            {
+                batteries -= 1;
+                usedEnergy = 0;
+            }
+            // print("Batteries: " + batteries);
+            // print("usedEnergy: " + usedEnergy);
+        }
+        else
+        {
+            lightObj.SetActive(false);
+        }
+    }
+
+    //Charges Flashlight when battery collides with it
+    public void OnTriggerEnter(Collider other){
+        if (other.tag == "Battery")
+        {
+            batteries += 1;
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        //If the flashlight is in the players hands inHands = True
+        if (other.tag == "Hands")
+            inHands = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        inHands = false;
+    }
+
+    public void ToggleSwitch()
+    {
+        if (inHands)
+        {
+            flashlightEnabled = !flashlightEnabled;
+            FixedUpdate();
+        }
+    }
+
+    public bool IsOn()
+    {
+        if(flashlightEnabled)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public const int startingHealth = 100;
+    public float currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    //Red color with 10% opacity
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    bool isDead;
+    bool damaged;
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    private void FixedUpdate()
+    {
+        //Flashes the red color when damaged
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        }
+        //Fades away the red color if damgaged
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        if (currentHealth <= 0 && !isDead)
+        {
+            Debug.Log("Dead!");
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        isDead = true;
+    }
+}
