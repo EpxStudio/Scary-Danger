a7da5f83ecf9fece5695bbd8affd595d623c2257
vruser <VRUser@SAAH-S271.iowa.uiowa.edu>
2018-04-13 19:30:32 -0500

    fix mistakes

---
+++ Assets/Animation/DangerAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerAnimation : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.StopPlayback();
+        anim.speed = 25;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.Play("Take 001");
+    }
+}
