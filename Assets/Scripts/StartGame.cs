e70c8225d4c80c3567b7167b2991ef4c01459ccf
vruser <VRUser@SAAH-S271.iowa.uiowa.edu>
2018-04-20 20:10:23 -0500

    "added start scene"

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public Button startText;
+
+    // Use this for initialization
+    void Start()
+    {
+        startText = startText.GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+
+    public void StartLevel()
+    {
+        SceneManager.LoadSceneAsync("mainScene");
+    }
+}
